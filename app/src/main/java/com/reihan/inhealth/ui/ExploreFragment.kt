c0b93ff62bb96f2d1f6bbc7a003c246c642d4af4
ReihanFatilla <reihanfatilla@gmail.com>
2022-03-24 09:57:02 +0700

    Fixxing Some Code(2)

--- app/src/main/java/com/reihan/inhealth/ui/ExploreFragment.kt
+++ app/src/main/java/com/reihan/inhealth/ui/ExploreFragment.kt
@@ -1,18 +1,14 @@
 package com.reihan.inhealth.ui
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout.HORIZONTAL
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.reihan.inhealth.R
 import com.reihan.inhealth.adapter.ArtikelAdapter
 import com.reihan.inhealth.adapter.ExploreNewsAdapter
-import com.reihan.inhealth.adapter.NewsAdapter
 import com.reihan.inhealth.data.DataArtikel
 import com.reihan.inhealth.data.DataNews
 import com.reihan.inhealth.databinding.FragmentExploreBinding
@@ -23,7 +19,6 @@ class ExploreFragment : Fragment() {
     private var _binding: FragmentExploreBinding? = null
     private val binding get() = _binding!!
 
-    @SuppressLint("WrongConstant")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,7 +28,7 @@ class ExploreFragment : Fragment() {
 
         binding.rvArtikelExplore.apply {
             setHasFixedSize(true)
-            layoutManager = LinearLayoutManager(activity, HORIZONTAL, false)
+            layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
             adapter = ArtikelAdapter(DataArtikel.dataExplore)
         }
         binding.rvBeritaExplore.apply {
--- app/src/main/java/com/reihan/inhealth/ui/MainFragment.kt
+++ app/src/main/java/com/reihan/inhealth/ui/MainFragment.kt
@@ -1,15 +1,11 @@
 package com.reihan.inhealth.ui
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.HorizontalScrollView
-import android.widget.LinearLayout.HORIZONTAL
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.reihan.inhealth.R
 import com.reihan.inhealth.adapter.ArtikelAdapter
 import com.reihan.inhealth.adapter.NewsAdapter
 import com.reihan.inhealth.data.DataArtikel
@@ -20,7 +16,6 @@ class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
 
-    @SuppressLint("WrongConstant")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -32,7 +27,7 @@ class MainFragment : Fragment() {
             adapter = NewsAdapter(DataNews.dataTrending)
         }
         binding.rvArtikelPopuler.apply {
-            layoutManager = LinearLayoutManager(activity, HORIZONTAL, false)
+            layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
             adapter = ArtikelAdapter(DataArtikel.dataPopuler)
         }
         return binding.root
